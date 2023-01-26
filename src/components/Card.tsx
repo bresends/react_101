857988d864eba429eecfc70cd13d75722abbe411
Your Name <you@example.com>
2023-01-26 14:11:03 -0300

    Fix: Tailwind Class Ordering

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 
 export const Card: FC<CardProps> = ({ title, description, image, link }) => {
   return (
-    <div className="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100  bg-opacity-50 shadow-2xl shadow-gray-600/10">
+    <div className="group rounded-3xl border border-gray-100 bg-white bg-opacity-50 p-6  shadow-2xl shadow-gray-600/10 sm:p-8">
       <Link to={link}>
         <div className="relative overflow-hidden rounded-xl">
           <img
@@ -22,14 +22,14 @@ export const Card: FC<CardProps> = ({ title, description, image, link }) => {
             className="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"
           />
         </div>
-        <div className="mt-6 relative">
+        <div className="relative mt-6">
           <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
             {title}
           </h3>
           <p className="mt-6 mb-8 text-gray-600">{description}</p>
           <button
             type="button"
-            className="inline-block px-6 py-2.5 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg hover:text-white focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            className="inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-tight shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
           >
             Detalhes
           </button>
